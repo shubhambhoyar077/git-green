d11743f21ba2035e90b652a8fbcdc4f5b7742985
shubham <shubham.bhoyar077@gmail.com>
2023-09-17 11:42:34 +0530

    fix import error

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App.tsx';
 import './index.css';
-import store from './redux/store.ts';
+import store from './redux/store.js';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
