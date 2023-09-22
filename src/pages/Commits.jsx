8a7207dd03bed7ebb4f12e17252d442c99bbeb68
shubham <shubham.bhoyar077@gmail.com>
2023-09-22 17:08:44 +0530

    Add repo in dropdown

--- src/pages/Commits.jsx
+++ src/pages/Commits.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchRepo } from '../redux/repo/repoSlice';
+
 const Commits = () => {
+  const { repo } = useSelector((state) => state.repo);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRepo({ method_data: { method: 'GET' } }));
+  }, []);
+
   return (
     <section>
       <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
@@ -18,11 +29,10 @@ const Commits = () => {
                 id="category"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
               >
-                <option selected="">Select category</option>
-                <option value="TV">TV/Monitors</option>
-                <option value="PC">PC</option>
-                <option value="GA">Gaming/Console</option>
-                <option value="PH">Phones</option>
+                <option selected>Select category</option>
+                {repo.map((repository) => (
+                  <option value={repository.id}>{repository.name}</option>
+                ))}
               </select>
             </div>
             <div className="sm:col-span-2">
