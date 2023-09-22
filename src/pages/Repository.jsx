7b28a63c0863a3a1dd6a526a30b9e88f3346eade
shubham <shubham.bhoyar077@gmail.com>
2023-09-22 22:04:46 +0530

    Add function for create repo

--- src/pages/Repository.jsx
+++ src/pages/Repository.jsx
@@ -1,8 +1,33 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchRepo } from '../redux/repo/repoSlice';
+import { useState } from 'react';
+
 const Repository = () => {
+  const [reponame, setReponame] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      method_data: {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ repository: { name: reponame } }),
+      },
+    };
+    dispatch(fetchRepo(data));
+  };
+
+  const handleChange = (e) => {
+    setReponame(e.target.value);
+  };
+
   return (
     <section>
       <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
-        <form action="#">
+        <form onSubmit={handleSubmit}>
           <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
             <div className="sm:col-span-2">
               <label
@@ -13,10 +38,12 @@ const Repository = () => {
               </label>
               <input
                 type="text"
-                name="UserName"
+                name="RepoName"
                 id="Repository Name"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Repository Name"
+                value={reponame}
+                onChange={handleChange}
                 required
               />
             </div>
