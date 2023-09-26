d85b542b88460eb518de9eb565e3323335e3a01d
shubham <shubham.bhoyar077@gmail.com>
2023-09-26 19:41:27 +0530

    Add logic in handleChange

--- src/pages/Commits.jsx
+++ src/pages/Commits.jsx
@@ -21,13 +21,23 @@ const Commits = () => {
     dispatch(fetchRepo({ method_data: { method: 'GET' } }));
   }, []);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(postScheduleCommit(scheduleCommit));
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setScheuleCommit({ ...scheduleCommit, [name]: value });
+  };
+
   return (
     <section>
       <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
         <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">
           Create new commit
         </h2>
-        <form action="#">
+        <form onSubmit={handleSubmit}>
           <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
             <div className="sm:col-span-2">
               <label
@@ -48,15 +58,17 @@ const Commits = () => {
             </div>
             <div className="sm:col-span-2">
               <label
-                htmlFor="name"
+                htmlFor="branch_name"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 Branch Name
               </label>
               <input
                 type="text"
-                name="name"
-                id="name"
+                name="branch_name"
+                id="branch_name"
+                onChange={handleChange}
+                value={scheduleCommit.branch_name}
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Type branch name"
                 required
@@ -64,15 +76,17 @@ const Commits = () => {
             </div>
             <div className="sm:col-span-2">
               <label
-                htmlFor="name"
+                htmlFor="file_name"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 File Path
               </label>
               <input
                 type="text"
-                name="name"
-                id="name"
+                name="file_name"
+                id="file_name"
+                onChange={handleChange}
+                value={scheduleCommit.file_name}
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="root/(file path)"
                 required
@@ -81,29 +95,33 @@ const Commits = () => {
 
             <div className="sm:col-span-2">
               <label
-                htmlFor="description"
+                htmlFor="content"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 File Content
               </label>
               <textarea
-                id="description"
-                // rows="8"
+                id="content"
+                name="content"
+                onChange={handleChange}
+                value={scheduleCommit.content}
                 className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Hello World!"
               ></textarea>
             </div>
             <div className="sm:col-span-2">
               <label
-                htmlFor="name"
+                htmlFor="commit_message"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 Commit Message
               </label>
               <input
                 type="text"
-                name="name"
-                id="name"
+                name="commit_message"
+                id="commit_message"
+                onChange={handleChange}
+                value={scheduleCommit.commit_message}
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Type Commit Message"
                 required
