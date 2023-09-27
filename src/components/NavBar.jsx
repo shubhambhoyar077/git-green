fe71f77a25a9cad96ac8178fed6b1db74350fe3c
shubham <shubham.bhoyar077@gmail.com>
2023-09-27 17:12:09 +0530

    Remove schedule link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -85,14 +85,6 @@ const NavBar = () => {
                 Commit
               </Link>
             </li>
-            <li>
-              <Link
-                to="schdulecommit"
-                className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Schedule
-              </Link>
-            </li>
           </ul>
         </div>
       </div>
