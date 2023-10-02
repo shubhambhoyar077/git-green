89771d67fb001e1625d99ddddc744aa893d7a530
shubham <shubham.bhoyar077@gmail.com>
2023-10-02 20:55:24 +0530

    Create simple confirmation page

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import ForgetPassword from './pages/ForgetPassword';
 import ConfirmationMail from './pages/ConfirmationMail';
+import ConfirmationPage from './pages/ConfirmationPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,6 +28,7 @@ const router = createBrowserRouter(
       <Route path="sign_in" element={<SignIn />} />
       <Route path="forget_password" element={<ForgetPassword />} />
       <Route path="confirmation_mail" element={<ConfirmationMail />} />
+      <Route path="confirmation_page" element={<ConfirmationPage />} />
     </Route>
   )
 );
