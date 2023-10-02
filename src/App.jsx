35b6dfa3d1c69f366b82494f533a827dc76067a5
shubham <shubham.bhoyar077@gmail.com>
2023-10-02 20:34:31 +0530

    Add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Home from './pages/Home';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import ForgetPassword from './pages/ForgetPassword';
+import ConfirmationMail from './pages/ConfirmationMail';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -25,6 +26,7 @@ const router = createBrowserRouter(
       <Route path="sign_up" element={<SignUp />} />
       <Route path="sign_in" element={<SignIn />} />
       <Route path="forget_password" element={<ForgetPassword />} />
+      <Route path="confirmation_mail" element={<ConfirmationMail />} />
     </Route>
   )
 );
