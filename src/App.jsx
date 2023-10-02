cc700556e6791f3f847947fea6d622780b6c7605
shubham <shubham.bhoyar077@gmail.com>
2023-10-02 19:40:20 +0530

    Add link for forget pass

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import ScheduleCommit from './pages/ScheduleCommit';
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
+import ForgetPassword from './pages/ForgetPassword';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -23,6 +24,7 @@ const router = createBrowserRouter(
       <Route path="schdulecommit" element={<ScheduleCommit />} />
       <Route path="sign_up" element={<SignUp />} />
       <Route path="sign_in" element={<SignIn />} />
+      <Route path="forget_password" element={<ForgetPassword />} />
     </Route>
   )
 );
