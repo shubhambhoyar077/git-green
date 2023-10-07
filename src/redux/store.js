f82b7ff5f3bb21b21b1036482470038287a0d201
shubham <shubham.bhoyar077@gmail.com>
2023-10-07 19:55:38 +0530

    Add auth slice in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import repositoryReducer from './repository/repositorySlice';
 import repoReducer from './repo/repoSlice';
+import authsReducer from './auths/authsSlice';
+import userauthReducer from './auths/userauthSlice';
 
 const store = configureStore({
   reducer: {
     repository: repositoryReducer,
     repo: repoReducer,
+    auths: authsReducer,
+    userAuth: userauthReducer,
   },
 });
 
